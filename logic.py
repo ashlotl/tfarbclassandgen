0ab91e272e17d1c2603fb63dcbee9eb1fa8cdefa
thesupremeliverwurst@gmail.com <thesupremeliverwurst@gmail.com>
2019-07-03 11:22:27 -0700

    That loss function doesn't seem to work very well at higher dimensions...

--- logic.py
+++ logic.py
@@ -1,10 +1,7 @@
 from __future__ import print_function
-import tensorflow as tf,numpy as np,math,pygame
+import tensorflow as tf,numpy as np,math
 from tensorflow.python import debug as tf_debug
 # tf.keras.backend.set_session(tf_debug.TensorBoardDebugWrapperSession(tf.Session(),'localhost:6064'))
-pygame.init()
-screen=pygame.display.set_mode((500,500))
-pygame.display.set_caption("Scary port v3084")
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
 #open some data, like the comedically ill-suited Star Wars script
 file=open("script","r")
@@ -15,89 +12,66 @@ numQuotes=10#int(len(wf)/(wordSize*quoteSize))#pretty obvious
 flattened=np.zeros(numQuotes*wordSize*quoteSize*128).reshape(numQuotes,wordSize*quoteSize*128)
 for quote in range(0,numQuotes):
     iterator=0
-    # print(quote)
     for word in range(0,quoteSize):
         for character in range(0,wordSize):
             for indicator in range(0,128):
                 if indicator==ord(wf[quote*quoteSize+word*wordSize+character]):
                     flattened[quote][iterator]=1.0
                 iterator+=1
-print(len(flattened))
-total=0
-epochid=0
-def fact(num):
-    if num!=1:
-        return num*fact(num-1)
 #define a (non-Expanse) spacing function that ensures each datapoint is arbitrarily as far from the other as possible.
-def check((ye,ype)):
-    if ye!=ype:
-        return (1, 1)
-    return (0,0)
-
 def invdistp(y,y_pred):
-    # print(y,y_pred)
-    # total=tf.Variable(initial_value=tf.constant(.1),trainable=False)
-    # if tf.map_fn(check,(y,y_pred))==(0,0):
-    #     return 0
-    # bad=True
-    # i=0
-    #
-    # for e in y:
-    #     if y[i]!=y_pred[i]:
-    #         bad=False
-    #         break
-    #     i+=1
-    # if bad:
-    #     return 0
-    print("SHAPE:",y.shape,y_pred.shape)
     lamb=lambda val : val
-    total=tf.square(tf.subtract(tf.map_fn(lamb,tf.abs(tf.subtract(y,y_pred))),.5))
-    # total2=tf.map_fn(lamb, tf.square(tf.subtract(y_pred,0.5)))
-    print("HERE:",total)
-    return tf.reduce_sum(total)
-# invdist=tf.contrib.eager.function(invdistp)
+    total=tf.square(tf.subtract(tf.map_fn(lamb,tf.abs(tf.subtract(y,y_pred))),.1))
+    return -((tf.reduce_sum(tf.map_fn(lamb,tf.abs(tf.subtract(y,y_pred))))))
 #define an arbitrary classifier
 print("Initializing classifier")
+numOut=10
 classifier=tf.keras.Sequential([
 tf.keras.layers.Dense(quoteSize*wordSize*128,activation='relu',input_shape=[quoteSize*wordSize*128]),
 tf.keras.layers.Dense(int(.5*quoteSize*wordSize*128),activation='sigmoid'),
-tf.keras.layers.Dense(int(2),activation='sigmoid')
+tf.keras.layers.Dense(int(.5*quoteSize*wordSize*128),activation='sigmoid'),
+tf.keras.layers.Dense(int(.5*quoteSize*wordSize*128),activation='sigmoid'),
+tf.keras.layers.Dense(int(numOut),activation='sigmoid')
 ])
-# classifier.build()
-# predictor=classifier
 print("Compiling")
 classifier.compile(optimizer='sgd',loss=invdistp)
 classifier.summary()
 print("Fitting")
-z=True
-pygame.display.flip()
-for epoch in range(0,500):
-    screen.fill((255,255,255))
+xs=np.zeros((1,int(quoteSize*wordSize*128)))
+classifierStatusQuo=np.zeros((len(flattened),numOut))
+numEpochs=500
+for epoch in range(0,numEpochs):
     for valin in range(0,len(flattened)):
-        xs=np.zeros((1,int(quoteSize*wordSize*128)))
         xs[0]=flattened[valin]
         ys1=classifier.predict_on_batch(xs)
+        if epoch==numEpochs-1:
+            classifierStatusQuo[valin]=ys1
         for valout in range(0,(len(flattened)+1)/2):
             if valin!=valout:
                 xs2=np.empty((1,int(quoteSize*wordSize*128)))
                 xs2[0]=flattened[valout]
+                print("Epoch",epoch+1,"/",numEpochs)
                 classifier.fit(xs2,ys1,epochs=1,batch_size=1)
-        pygame.draw.circle(screen,(0,0,0),(int(200*(ys1[0][0])),int(200*(ys1[0][1]))),2,0)
-    pygame.display.flip()
-#custom loss that allows one prediction to be compared to many expecteds with a 'nearest' behaviour
-def lossf(y,y_pred):
-    return 1-y_pred
-#the input to generator could be different and is not correlated to the output from classifier, we simply do this for some symmetry
+
 print("Initializing generator")
+numIn=int(.1*quoteSize*wordSize*128)
 generator=tf.keras.Sequential([
-tf.keras.layers.Dense(int(.1*quoteSize*wordSize*128),activation='relu',input_shape=[int(.1*quoteSize*wordSize*128)]),
+tf.keras.layers.Dense(numIn,activation='relu',input_shape=[int(.1*quoteSize*wordSize*128)]),
 tf.keras.layers.Dense(int(.3*quoteSize*wordSize*128),activation='sigmoid'),
 tf.keras.layers.Dense(int(.5*quoteSize*wordSize*128),activation='sigmoid'),
 tf.keras.layers.Dense(int(.8*quoteSize*wordSize*128),activation='sigmoid'),
 tf.keras.layers.Dense(quoteSize*wordSize*128,activation='sigmoid')
 ])
 print("Compiling")
-generator.compile(optimizer='adam',loss=lossf,metrics=['accuracy'])
+generator.compile(optimizer='sgd',loss=lossf,metrics=['accuracy'])
 print("Fitting")
-generator.fit(tf.ones([1,200]),tf.zeros([1,200]),epochs=10,steps_per_epoch=1,verbose=1)
-print(generator.predict(tf.ones([1,200]),steps=1))
+xseeds=np.zeros((len(flattened),numIn))
+for i in range(0,len(flattened)):
+    for j in range(0,numIn):
+        xseeds[i][j]=math.random()
+generator.fit(xseeds,classifierStatusQuo,epochs=100,steps_per_epoch=1,verbose=1)
+while True:
+    xseed=np.zeros((1,numIn))
+    for i in range(0,numIn):
+        xseed[0][i]=math.random()
+    print(generator.predict(xseed))
