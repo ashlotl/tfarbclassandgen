52e709a9bf41762a2dfe5f0812c07d501926e872
thesupremeliverwurst@gmail.com <thesupremeliverwurst@gmail.com>
2019-07-03 19:54:06 -0700

    Now faced by OOM

--- logic.py
+++ logic.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import tensorflow as tf,numpy as np,math,random
 from tensorflow.python import debug as tf_debug
+tf.enable_eager_execution()
 # tf.keras.backend.set_session(tf_debug.TensorBoardDebugWrapperSession(tf.Session(),'localhost:6064'))
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
 #open some data, like the comedically ill-suited Star Wars script
@@ -39,7 +40,7 @@ classifier.summary()
 print("Fitting")
 xs=np.zeros((1,int(quoteSize*wordSize*128)))
 classifierStatusQuo=np.zeros((len(flattened),numOut))
-numEpochs=100
+numEpochs=10
 for epoch in range(0,numEpochs):
     for valin in range(0,len(flattened)):
         xs[0]=flattened[valin]
@@ -51,7 +52,7 @@ for epoch in range(0,numEpochs):
                 xs2=np.empty((1,int(quoteSize*wordSize*128)))
                 xs2[0]=flattened[valout]
                 print("Epoch",epoch+1,"/",numEpochs)
-                classifier.fit(xs2,ys1,epochs=1,batch_size=1)
+                classifier.fit(xs2,ys1,epochs=2,batch_size=1)
 
 print("Initializing generator")
 numIn=int(.1*quoteSize*wordSize*128)
@@ -65,11 +66,23 @@ tf.keras.layers.Dense(quoteSize*wordSize*128,activation='sigmoid')
 print("Compiling")
 generator.compile(optimizer='sgd',loss='mean_squared_error',metrics=['accuracy'])
 print("Fitting")
-xseeds=np.zeros((len(flattened),numIn))
+xseeds=np.zeros((len(flattened),1,numIn))
 for i in range(0,len(flattened)):
     for j in range(0,numIn):
-        xseeds[i][j]=random.random()
-generator.fit(xseeds,classifierStatusQuo,epochs=100,steps_per_epoch=1,verbose=1)
+        xseeds[i][0][j]=random.random()
+numGenEpochs=11
+expected=np.zeros((1,quoteSize*wordSize*128))
+expected[0][0]=-1
+for oepoch in range(0,numGenEpochs):
+    for epoch in range(0,len(flattened)):
+        attempt=classifier.predict_on_batch(generator.predict_on_batch(xseeds[epoch]))
+        for point in range(0,len(flattened)):
+            cost=tf.reduce_sum(tf.square(tf.subtract(classifierStatusQuo[point],attempt)))
+            set=lambda:cost
+            ignore=lambda:expected[0][0]
+            expected[0][0]=tf.cond(cost<expected[0][0],set,ignore)
+            expected[0][0]=tf.cond(expected[0][0]==-1,set,ignore)
+        generator.fit(xseeds[epoch],expected,epochs=2,steps_per_epoch=1,verbose=1)
 while True:
     xseed=np.zeros((1,numIn))
     for i in range(0,numIn):
